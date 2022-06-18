2a53cb0ecb77264c549ae24443a0bd6a6464e271
Zidane Innis <zidaneinnis@gmail.com>
2022-06-18 12:07:53 -0500

    Top-Level await (ES2022)

--- script.js
+++ script.js
@@ -20,3 +20,24 @@ add('bread', 2);
 add('apples', 2);
 
 console.log(cart);
+
+// console.log('Start Fetching');
+// const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+// const data = await res.json();
+// console.log(data);
+// console.log('Something');
+
+const getLastPost = async function () {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const data = await res.json();
+  console.log(data);
+  return { title: data.at(-1).title, text: data.at(-1).body };
+};
+const lastPost = getLastPost();
+console.log(lastPost);
+
+// Not very clean
+// lastPost.then(last => console.log(last));
+
+const lastPost2 = await getLastPost();
+console.log(lastPost2);
